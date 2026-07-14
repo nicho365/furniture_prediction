b693bafc869d1fe413c5f3b8d13ffe30582ad1e0
nicho365 <nicholasclvn@gmail.com>
2026-07-14 15:57:35 +0700

    ui changes

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import pickle
 import gdown
 import os
 
-# Konfigurasi Halaman (Tab browser)
 st.set_page_config(page_title="Furniture Classfication", layout="centered")
 
 @st.cache_resource
@@ -16,7 +15,7 @@ def load_model_from_drive():
     output_path = 'model_furniture.pkl'
     
     if not os.path.exists(output_path):
-        with st.spinner("Mengunduh model dari Google Drive (Hanya dilakukan sekali)..."):
+        with st.spinner("Downloading model...(only for first time)"):
             gdown.download(url, output_path, quiet=False)
     
     with open(output_path, 'rb') as file:
@@ -26,38 +25,45 @@ def load_model_from_drive():
 try:
     model = load_model_from_drive()
 except Exception as e:
-    st.error(f"Gagal memuat model: {e}")
+    st.error(f"Error while load model: {e}")
     st.stop()
 class_names = ['bed', 'chair', 'sofa', 'swivelchair', 'table']
 
-def preprocess_and_predict(image, model):
-    # 1. Mendapatkan ukuran yang diharapkan model (misal: (None, 150, 150, 3))
-    # Kita ambil indeks ke-1 (tinggi) dan ke-2 (lebar)
+def predict(image, model):
     input_shape = model.input_shape
     target_size = (input_shape[1], input_shape[2]) 
-    
-    # 2. Resize gambar secara otomatis mengikuti target_size dari model
     img = image.resize(target_size)
-    img_array = img_to_array(img)
-    
-    # 3. Menambah dimensi untuk batch (1, tinggi, lebar, 3)
-    img_array = np.expand_dims(img_array, axis=0)
-    
-    # 4. Normalisasi (membagi nilai piksel dengan 255)
+    img_array = img_to_array(img)    
+    img_array = np.expand_dims(img_array, axis=0)    
     img_array = img_array / 255.0
     
-    # 5. Melakukan prediksi
     predictions = model.predict(img_array)
     predicted_class = class_names[np.argmax(predictions)]
     confidence = np.max(predictions)
-    
+
     return predicted_class, confidence
 
-# UI Aplikasi
 st.title("Furniture Classification")
 st.write("This model prediction use Convolutional Neural Network VGG16 to predict.")
 
+with st.expander("Performance Analysis Model (Classification Report)"):
+    st.markdown("""
+    This model was trained using the *VGG16* architecture with Transfer Learning. Here is a summary of the evaluation results:
+    
+    *   Overall accuracy: `95%`
+    *   Key Strengths: The model has the highest Precision and Recall scores in the bed and sofa class. This is because the object has a massive shape and very distinctive visual features compared to other categories..
+    *   Limitations: This model can make wrong predictions for images that are not furniture or even furniture images.
+    """)
+
 st.markdown("---")
+
+st.info("""
+   **Photo Upload Guide for More Accurate Results:**
+1. Focus: Make sure the furniture (Bed, Chair, Sofa, Swivelchair, or Table) is the main object and appears the largest in the photo.
+2. One Object: Avoid photos that show lots of furniture stacked on top of each other.
+3. Lighting and Angles: Use bright lighting. Take photos from a natural angle (e.g., photographing a chair from the front/side, not from above).
+4. Not Random Images: The system has a tolerance limit. Images outside the five furniture categories above will be automatically rejected.
+""")
 
 uploaded_file = st.file_uploader("Choose or drag photo file here", type=["jpg", "jpeg", "png"])
 
@@ -69,7 +75,7 @@ if uploaded_file is not None:
     
     if st.button("PREDICT"):
         with st.spinner("Loading..."):
-            predicted_class, confidence = preprocess_and_predict(image, model)
+            predicted_class, confidence = predict(image, model)
     
             THRESHOLD = 0.85 
             
@@ -79,3 +85,8 @@ if uploaded_file is not None:
             else:
                 st.success(f"**Prediction results:** {predicted_class.capitalize()}")
                 st.info(f"**Confidence level (Confidence):** {confidence:.2%}")
+
+st.markdown("---")
+st.markdown(
+    "More infos and :star: at [github.com/nicho365/furniture_prediction](https://github.com/nicho365/furniture_prediction)"
+)
